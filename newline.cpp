2ebff426846dde498808e4663b23c6c063a903f3
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-08 13:27:53 +0530

    Creating New Lines Using CPP

---
+++ newline.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "This is first line \n"; // printing new line
+    cout << "Print the new line \n"; 
+    cout << "This is third line \n\n"; //line brake
+    cout << "This if fifth line \n";
+    cout << "This is sixth line" << endl; //an another way for printing new line
+    cout << "This seventh line";
+    return 0;
+}
