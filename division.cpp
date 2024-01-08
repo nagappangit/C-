bd949dcab992ab63b425e8e688c3c064061931b9
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-08 10:15:48 +0530

    Simple Division CPP Program

---
+++ division.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//simple division cpp program
+void division(){
+    int a,b,c;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+    c = a - b;
+    cout << "The result is: " << c;
+}
+
+int main(){
+    division();
+    return 0;
+}
