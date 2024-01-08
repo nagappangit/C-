33856d25b91b7d7f21b0b31976f0c838a26bcaf7
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-08 13:10:48 +0530

    CPP Syntax

---
+++ cppsyntax.cpp
@@ -0,0 +1,7 @@
+#include <iostream>// Header file library
+using namespace std;// Use names for objects and variables from the standard library
+// CPP ignores white space
+int main(){// This is called a function
+    cout << "Engineering World!!!";// cout is an object used together with the insertion operator (<<) to print text / CPP statement ends with a semicolon
+    return 0;// return 0 ends the main function
+}
