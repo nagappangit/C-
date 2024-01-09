f19e8c26f54a71d782b0dcc4d60a9bc0dad9ef68
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-09 20:47:01 +0530

    CPP Comments

---
+++ cppcomments.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+//c++ comments
+int main (){
+    //single line comment or short comments
+    cout << "Welcome to learn c++";
+    /* Multi line comment  or long comments */ 
+    return 0;
+}
