ac525036fff25328de02013fa3f045c2090e4768
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-10 10:37:07 +0530

    CPP Variables

---
+++ cppvariables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+//c++ variables
+//Declare Varibales
+void declareVaribales(){
+    int numValue = 30; // Integer (Full Value without decimal value)
+    cout << numValue << "\n";
+    double floatValue = 3.3; // Floating point value (Full value with decimal value)
+    cout << floatValue << "\n";
+    char charLetter = 'N'; // Character
+    cout << charLetter << "\n";
+    string stringText = "Welcome to Learn C++"; // String (Group of texts)
+    cout << stringText << "\n";
+    bool trueResult = true; // Boolean (True or False)
+    cout << trueResult;
+}
+int main(){
+    declareVaribales();
+    return 0;
+}
