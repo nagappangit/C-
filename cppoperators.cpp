8e228510ce01fdc25ee07ef71c2cb8f01bdd745e
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-17 18:32:09 +0530

    Update cppoperators.cpp

--- cppoperators.cpp
+++ cppoperators.cpp
@@ -23,7 +23,56 @@ void arithmeticoperators(){
     c = --b;
     cout << "The decrement result is: " << c << endl;
 }
+//Assignment Operators
+void assignmentoperators(){
+    int a, b;
+    cout << "Please enter the value: ";
+    cin >> a;
+    b = a;
+    cout << b << endl;
+    b += a;
+    cout << b << endl;
+    b = b + a;
+    cout << b << endl;
+    b -= a;
+    cout << b << endl;
+    b = b - a;
+    cout << b << endl;
+    b *= a;
+    cout << b << endl;
+    b = b * a;
+    cout << b << endl;
+    b /= a;
+    cout << b << endl;
+    b = b / a;
+    cout << b << endl;
+    b %= a;
+    cout << b << endl;
+    b = b % a;
+    cout << b << endl;
+    b &= a;
+    cout << b << endl;
+    b = b & a;
+    cout << b << endl;
+    b |= a;
+    cout << b << endl;
+    b = b | a;
+    cout << b << endl;
+    b ^= a;
+    cout << b << endl;
+    b = b ^ a;
+    cout << b << endl;
+    b >>= a;
+    cout << b << endl;
+    b = b >> a;
+    cout << b << endl;
+    b <<= a;
+    cout << b << endl;
+    b = b << a;
+    cout << b << endl;
+}
 int main(){
     arithmeticoperators();
+    assignmentoperators();
     return 0;
 }
