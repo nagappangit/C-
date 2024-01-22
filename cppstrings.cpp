3d3f95342782fb595233adb7a35e4587125e077e
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-22 12:57:42 +0530

    Update cppstrings.cpp

--- cppstrings.cpp
+++ cppstrings.cpp
@@ -8,7 +8,17 @@ void stringtext(){
     cin >> name;
     cout << "Your Name is " << name;
 }
+//C++ String Concatenation
+void stringconcatenation(){
+    string firstname, lastname;
+    cout << "Enter your first name: ";
+    cin >> firstname;
+    cout << "Enter your last name: ";
+    cin >> lastname;
+    cout << "Your Name is " << firstname + " " + lastname; // + operator can be used between one or more strings
+}
 int  main(){
     stringtext();
+    stringconcatenation();
     return 0;
 }
