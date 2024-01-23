051a6f7664a2001c4789516a269a0cbc83b0f23b
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-01-23 13:26:54 +0530

    Update cppstrings.cpp

--- cppstrings.cpp
+++ cppstrings.cpp
@@ -17,8 +17,26 @@ void stringconcatenation(){
     cin >> lastname;
     cout << "Your Name is " << firstname + " " + lastname; // + operator can be used between one or more strings
 }
+//C++ Numbers and Strings
+//C++ uses the + operator for both addition and concatenation
+void numbersandstrings(){
+    int a,b,c;
+    string firstname, lastname;
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+    c = a + b; // Numbers are added
+    cout << "The result is: " << c << endl;
+    cout << "Enter your first name: ";
+    cin >> firstname;
+    cout << "Enter your last name: ";
+    cin >> lastname;
+    cout << "Your Name is " << firstname + " " + lastname; // Strings are concatenated
+}
 int  main(){
-    stringtext();
-    stringconcatenation();
+    //stringtext();
+    //stringconcatenation();
+    numbersandstrings();
     return 0;
 }
