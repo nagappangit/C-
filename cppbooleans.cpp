e49d96294c9c853111849314716feb0a9460784b
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-02-07 17:49:47 +0530

    CPP Booleans

---
+++ cppbooleans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+//C++ Booleans
+void booleans(){    
+    bool a = true;
+    cout << a << endl; // Output 1 (true)
+    bool b = false;
+    cout << b << endl; // Output 0 (false)
+}
+int main(){
+    booleans();
+}
