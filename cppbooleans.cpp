1dbcf0637fa44a3d1cf95b2893ee4f37aecd1a87
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-02-08 12:15:52 +0530

    Update cppbooleans.cpp

--- cppbooleans.cpp
+++ cppbooleans.cpp
@@ -10,6 +10,21 @@ void booleans(){
     bool b = false;
     cout << b << endl; // Output 0 (false)
 }
+//C++ Boolean Expressions
+void booleanexpressions(){
+    string firstName, lastName;
+    cout << "Enter your first name: ";
+    cin >> firstName;
+    cout << "Enter your last name: ";
+    cin >> lastName;
+    if (firstName != lastName){ // Boolean expression returns a boolean value that is either 1 (true) or 0 (false)
+        cout << "Your Name is: " << firstName << " " << lastName;
+    }
+    else{
+        cout << "Your first name and last name never should be the same.";
+    }
+}
 int main(){
     booleans();
+    booleanexpressions();
 }
