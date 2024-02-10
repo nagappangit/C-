ac2a6f410cadae1c7ab456a705ab7557a310968e
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-02-10 12:19:36 +0530

    Update cppconditions.cpp

--- cppconditions.cpp
+++ cppconditions.cpp
@@ -14,7 +14,21 @@ void ifelse(){
         cout << "You are the eligible person for voting !!!";
     }
 }
+//C++ Else
+void cppelse(){
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age >= 18){ // Use if to specify a block of code to be executed, if a specified condition is true
+        cout << "You are an eligible person to vote in India!!!";
+    }
+    else{ //  Use else to specify a block of code to be executed if the condition is false
+
+        cout << "You are not an eligible person to vote in India!!!";
+    }
+}
 int main(){
-    ifelse();
+    //ifelse();
+    cppelse();
     return 0;
 }
