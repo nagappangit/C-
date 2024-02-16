0520c87fb35565ba51d26ffe887d1ab4bae377b3
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-02-16 11:09:49 +0530

    Update cppwhileloop.cpp

--- cppwhileloop.cpp
+++ cppwhileloop.cpp
@@ -15,7 +15,23 @@ void whileloop(){
         age ++;
     }    
 }
+//C++ Do/While Loop
+void dowhileloop(){
+    int age;
+    cout << "Please Enter your age:";
+    cin >> age;
+    do{
+        if (age >= 18 ){
+        cout << "You are the eligible person to vote in India !!!" << endl;
+        }
+        else{
+        cout << "You are not the eligible person to vote in India !!!" << endl;
+        }
+        age ++;
+    }while (age <= 18);    
+}
 int main(){
-     whileloop();
-     return 0;
+    whileloop();
+    dowhileloop();
+    return 0;
 }
