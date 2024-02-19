6c7cf6a32a626b3425a791a3dc24fffd43e97c3f
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-02-19 18:49:53 +0530

    CPP For Loop

---
+++ cppforloop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+//C++ For Loop
+void forloop(){
+    int a;
+    cout << "Election 2024 Title how many times to print:";
+    cin >> a;
+    for(int i = 0; i < a; i++){
+        cout << " Election - 2024" << endl;
+    }
+}
+int main(){
+    forloop();
+    return 0;
+}
