bea9a27567a65116152d771944e0bf9e9b02efc9
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-02-22 10:47:52 +0530

    CPP Arrays

---
+++ cpparrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+//C++ Arrays
+void arrays(){
+    string election[5] = {"facebook", "twitter", "linkedin", "instagram", "pinteret"};
+    cout << election[0] << endl;
+    cout << election[1] << endl;
+    cout << election[2] << endl;
+    cout << election[3] << endl;
+    cout << election[4] << endl;
+}
+int main(){
+    arrays();
+    return 0;
+}
