0dd6a33c3a8827757377597748359c2b135fb548
Nagappan Kumaresan <nagarajeshbe@gmail.com>
2024-09-07 23:00:54 +0530

    C++ Array Size

--- cpparrays.cpp
+++ cpparrays.cpp
@@ -52,10 +52,31 @@ void omitelementseondeclaration(){
         cout << numbers[i] << " " << endl;
     }
 }
+// C++ Array Size
+void arraysize(){
+    // Get the Size of an Array
+    // Declare an array of 5 integers
+    int numbers[] = {10, 20, 30, 40, 50};
+
+    // Calculate the size of the array
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    // Print the size of the array
+    cout << "The size of the array is: " << size << endl;
+
+    // Loop Through an Array with sizeof()
+
+    // Loop through the array using the calculated size
+    cout << "Array elements: ";
+    for (int i = 0; i < size; ++i) {
+        cout << numbers[i] << " " << endl;
+    }
+}
 int main(){
     // arrays();
     // arraysandloops();
     // omitarraysize();
-    omitelementseondeclaration();
+    // omitelementseondeclaration();
+    arraysize();
     return 0;
 }
